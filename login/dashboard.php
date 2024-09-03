2d9c841b78821cb03c3c539bbac568a5a34b73f4
ShaunPunnenChandy <shaun2821.ak46@gmail.com>
2024-09-03 21:08:36 +0530

    update dashboard.php

--- login/dashboard.php
+++ login/dashboard.php
@@ -31,33 +31,29 @@ if (!isset($_SESSION['email'])) {
             <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light">
                 <div class="position-sticky">
                     <h4 class="text-center my-4">SonicScholar</h4>
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">
-                                <i class="fa fa-home"></i> Home
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <i class="fa fa-book"></i> My Courses
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <i class="fa fa-calendar"></i> Schedule
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <i class="fa fa-cogs"></i> Settings
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php">
-                                <i class="fa fa-sign-out"></i> Logout
-                            </a>
-                        </li>
-                    </ul>
+                    <div class="d-grid gap-2">
+                        <a class="btn btn-primary btn-block mb-2" href="tutorials.php">
+                            <i class="fa fa-book"></i> Course Materials
+                        </a>
+                        <a class="btn btn-secondary btn-block mb-2" href="todo_list.php">
+                            <i class="fa fa-list"></i> To-Do List
+                        </a>
+                        <a class="btn btn-warning btn-block mb-2" href="virtualkeyboard/index.php">
+                            <i class="fa fa-music"></i> Musical Keyboard
+                        </a>
+                        <a class="btn btn-info btn-block mb-2" href="ebooks.php">
+                            <i class="fa fa-book"></i> e-Books
+                        </a>
+                        <a class="btn btn-success btn-block mb-2" href="feedback.php">
+                            <i class="fa fa-comments"></i> Feedback
+                        </a>
+                        <a class="btn btn-primary btn-block mb-2" href="song_search.php">
+                            <i class="fa fa-search"></i> Song Search
+                        </a>
+                        <a class="btn btn-danger btn-block mb-2" href="logout.php">
+                            <i class="fa fa-sign-out"></i> Logout
+                        </a>
+                    </div>
                 </div>
             </nav>
 
@@ -73,22 +69,111 @@ if (!isset($_SESSION['email'])) {
                 </div>
 
                 <div class="row">
+                    <!-- Welcome Card -->
                     <div class="col-md-12">
-                        <div class="card">
+                        <div class="card mb-4">
                             <div class="card-body">
                                 <h3 class="card-title">Welcome, <?php echo htmlspecialchars($_SESSION['email']); ?>!</h3>
                                 <p class="card-text">Explore your courses, manage your schedule, and update your settings here.</p>
-                                
-                                <!-- Example Course List -->
-                                <h4>My Courses</h4>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <!-- Recent Activities -->
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <h4>Recent Activities</h4>
+                            </div>
+                            <div class="card-body">
                                 <ul class="list-group">
-                                    <li class="list-group-item">Introduction to Music Theory</li>
-                                    <li class="list-group-item">Advanced Musical Keyboard</li>
-                                    <li class="list-group-item">Digital Audio Workstation Basics</li>
+                                    <li class="list-group-item">Completed "Introduction to Music Theory" quiz</li>
+                                    <li class="list-group-item">Uploaded assignment for "Advanced Musical Keyboard"</li>
+                                    <li class="list-group-item">Viewed lecture notes for "Digital Audio Workstation Basics"</li>
                                 </ul>
                             </div>
                         </div>
                     </div>
+
+                    <!-- Upcoming Deadlines -->
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <h4>Upcoming Deadlines</h4>
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    <li class="list-group-item">Assignment due for "Digital Audio Workstation Basics" - Sep 10, 2024</li>
+                                    <li class="list-group-item">Final project submission for "Advanced Musical Keyboard" - Sep 15, 2024</li>
+                                    <li class="list-group-item">Quiz for "Introduction to Music Theory" - Sep 12, 2024</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <!-- Course Progress -->
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <h4>Course Progress</h4>
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    <li class="list-group-item">
+                                        Introduction to Music Theory 
+                                        <div class="progress">
+                                            <div class="progress-bar" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
+                                        </div>
+                                    </li>
+                                    <li class="list-group-item">
+                                        Advanced Musical Keyboard 
+                                        <div class="progress">
+                                            <div class="progress-bar" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%</div>
+                                        </div>
+                                    </li>
+                                    <li class="list-group-item">
+                                        Digital Audio Workstation Basics 
+                                        <div class="progress">
+                                            <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">90%</div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Announcements -->
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <h4>Announcements</h4>
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    <li class="list-group-item">New module released for "Digital Audio Workstation Basics"</li>
+                                    <li class="list-group-item">Office hours updated for "Advanced Musical Keyboard"</li>
+                                    <li class="list-group-item">Webinar on music theory scheduled for Sep 20, 2024</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Quick Links -->
+                    <div class="col-md-12">
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <h4>Quick Links</h4>
+                            </div>
+                            <div class="card-body d-flex flex-wrap gap-2">
+                                <a class="btn btn-outline-primary" href="profile.php">Profile</a>
+                                <a class="btn btn-outline-primary" href="settings.php">Settings</a>
+                                <a class="btn btn-outline-primary" href="grades.php">Grades</a>
+                                <a class="btn btn-outline-primary" href="forum.php">Discussion Forum</a>
+                                <a class="btn btn-outline-primary" href="support.php">Support</a>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </main>
         </div>
